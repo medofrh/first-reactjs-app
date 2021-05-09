78d88f60bd8d6a7fa4bfaf15969436a9586f43e1
medofrh <medo0o0frh@gmail.com>
2021-05-10 01:16:37 +0200

    try components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/menuComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">MeDo</NavbarBrand>
         </div>
-      </Navbar>  
+      </Navbar>
+      <Menu />
     </div>
   );
 }
